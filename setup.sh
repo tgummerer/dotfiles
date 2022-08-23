3741f815ef9a2eaed2b2d28a16b0399900a144b6
Thomas Gummerer <tgummerer@kangaroo.intra.tgummerer.com>
2022-08-23 16:09:47 +0200

    add zsh setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ dir=$(pwd)
 ln -sf ${dir}/.mbsyncrc ~/.mbsyncrc
 mkdir -p ~/.config/kitty
 ln -sf ${dir}/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ${dir}/.zshrc ~/.zshrc
+ln -sf ${dir}/.zimrc ~/.zimrc
